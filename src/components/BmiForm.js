87a71df14a91f333ece138970a59636022cd7376
riccar <ricardocarballo@gmail.com>
2018-01-10 14:15:22 +1100

    Completed app logic

--- src/components/BmiForm.js
+++ src/components/BmiForm.js
@@ -1,5 +1,7 @@
 //Class based component BmiForm. Form to request weight and height to calculate BMI
 
+//Import lodash to delay the callback function so the app waits until the user type the values in the form
+import _ from 'lodash';
 //Component within Curly brackets means import React, create a Component variable and initialize it with the React.Component property
 import React, { Component } from 'react';
 
@@ -14,14 +16,16 @@ class BmiForm extends Component {
     //Initializing the state by creating a new object and assign it to the state attribute of the class
     //Add the property term to the object as empty, to record the search term that is the value typed into the text box
     this.state = { 
-      weight: 75,
-      height: 175
+      weight: 75,//props.weight,
+      height: 175//props.height
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(event) {
+    
     const target = event.target;
     const name = target.name;
     const value = target.value;
@@ -29,8 +33,24 @@ class BmiForm extends Component {
     this.setState({
       [name]: value
     })
+    console.log(this.state);
+
+    //this.props.OnSubmitForm(this.state.weight, this.state.height);
+  }
+
+  handleSubmit(event) {
     
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+   
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state);
+
     this.props.OnSubmitForm(this.state.weight, this.state.height);
+    event.preventDefault();
   }
   
   //Returns a controlled component
@@ -38,35 +58,33 @@ class BmiForm extends Component {
     //as in classic html use the onChange property of the element to define the onChange function and provide the this.<functionName> defined in the class (within curly bracers)
     return (
       <div>
-        <label>
-          Your weight:
-          <input
-            name="weight"
-            type="number" 
-            value={this.state.weight}
-            //event.target.value has the input value
-            onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Your height:
-          <input
-            name="height" 
-            type="number" 
-            value={this.state.height}
-            //event.target.value has the input value
-            onChange={this.handleInputChange} />
-        </label>
-          
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Your weight:
+            <input
+              name="weight"
+              type="number"
+              required={true} 
+              value={this.state.weight}
+              //event.target.value has the input value
+              onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Your height:
+            <input
+              name="height" 
+              type="number"
+              required={true} 
+              value={this.state.height}
+              //event.target.value has the input value
+              onChange={this.handleInputChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>  
       </div>
     );
   }
 
-  //event handler on + <ElementName> + <EventName>. Add the event parameter
-  onInputChange(term) {
-    //{term} equals to {term: term}
-    this.setState({term});
-    this.props.OnSearchTermChange(term);
-  }
 }
 
 export default BmiForm;
--- src/components/index.js
+++ src/components/index.js
@@ -1,38 +1,39 @@
+
 //Import react so it can be used inside this module. The transpiler will take care of provide access to it. 
-import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import BmiForm from './BmiForm';
 import Result from './Result';
 
+//Main application class
 class BmiCalculator extends Component {
 
   constructor(props) {
     super(props);
 
     //Initialize component state 
-    this.state = { 
-      bmi: null
-    };
+    this.state = { bmi: null };
 
+  }
+
+  componentDidMount() {
     //showing default bmi 
     this.CalculateBmi(75, 175);
-    
   }
 
   CalculateBmi(weight, height) {
-    var bmi =  weight / height;
-    bmi = bmi / height;
-  
-    this.setState({bmi});
+    var bmi =  Math.pow((height / 100), 2);
+    bmi = weight / bmi;
+    this.setState({bmi: bmi});
   }
 
   render() {
+
     return (
       <div className="bmi-calculator">
-        <BmiForm OnSubmitForm={this.CalculateBmi} />
-        <Result bmi={this.state.bmi} />
+        <BmiForm OnSubmitForm={(width, height) => this.CalculateBmi(width, height)} />
+        <Result bmi={Math.round(this.state.bmi * 100) / 100} />
       </div>
     );
   }
